72062ce382015278bbcaacc24d7542840896ced6
ronaldofernandus <ronaldofernandus10@gmail.com>
2022-06-22 16:06:28 +0700

    getListJobsByWord

--- backend/controller/jobsController.js
+++ backend/controller/jobsController.js
@@ -1,4 +1,4 @@
-const axios = require("axios");
+const axios = require("axios").default;
 const URL = "http://dev3.dansmultipro.co.id/api/recruitment/positions.json";
 class jobsController {
   static async getListJobs(req, res) {
@@ -7,12 +7,41 @@ class jobsController {
         method: "GET",
         url: URL,
       });
+
       res.status(200).json(getListJobs.data);
-      console.log(data)
     } catch (error) {
-      console.log(error);
+      res.status(500).json(error);
     }
   }
+
+  static async getJobsByWord(req, res) {
+    try {
+      let description = req.query.description;
+      let location = req.query.location;
+      let getJobsByWord = await axios({
+        method: "GET" + "?",
+        url: URL,
+      });
+
+      res.status(200).json(getJobsByWord[(description, location)]);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+
+  // static async getJobsByWord(req, res) {
+  //   try {
+  //     const queryParameter=req.query;
+  //     let getJobsByWord = await axios({
+  //       method: "GET",
+  //       url: URL + { params: { answer: 42 } },
+  //     });
+
+  //     res.status(200).json(getJobsByWord.queryParameter);
+  //   } catch (error) {
+  //     res.status(500).json(error);
+  //   }
+  // }
 }
 
 module.exports = jobsController;
--- backend/routes/jobsRoute.js
+++ backend/routes/jobsRoute.js
@@ -2,6 +2,7 @@ const jobsRoute = require("express").Router();
 const jobsController = require("../controller/jobsController")
 
 jobsRoute.get('/list',jobsController.getListJobs)
+jobsRoute.get('/findByWord',jobsController.getJobsByWord)
 
 
 module.exports =jobsRoute
